d632f751e7d696937c7817036e0ca8e43cc86707
Franco Carrizo <franco.carrizo@bitlogic.io>
2022-04-16 21:19:43 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,34 @@
 module.exports = {
   siteMetadata: {
     title: `MiFirstSite`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
+    'gatsby-plugin-mdx',
+    'gatsby-transformer-sharp',
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true
+      }
+    }
+  ]
 }
---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,47 @@
+import { graphql } from 'gatsby'
+import * as React from 'react'
+import Layout from '../../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { Box } from '@chakra-ui/react'
+
+const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <Box bg='tomato' w='100%' p={4} color='purple.500'>
+        This is the Box
+      </Box>
+    </Layout>
+  )
+}
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      body
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
+
+export default BlogPost
