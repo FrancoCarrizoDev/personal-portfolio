c9d68eae040c9a0f14357649642292849e4a9cfc
FrancoCarrizoDev <francoadrianc@gmail.com>
2022-10-28 20:49:20 -0300

    change navbar colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,171 +1,214 @@
 import {
-  Box,
-  Flex,
-  Text,
-  IconButton,
-  Stack,
-  Collapse,
-  Link,
-  useBreakpointValue,
-  useDisclosure,
-  Container,
-  ScaleFade,
-  Icon
-} from '@chakra-ui/react'
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-import React from 'react'
-import { Fade } from 'react-awesome-reveal'
-import { useScrollPosition } from '../hooks/useScrollPosition'
-import { SiVisualstudiocode } from 'react-icons/si'
+	Box,
+	Flex,
+	Text,
+	IconButton,
+	Stack,
+	Collapse,
+	Link,
+	useBreakpointValue,
+	useDisclosure,
+	Container,
+	ScaleFade,
+	Icon,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { useScrollPosition } from "../hooks/useScrollPosition";
+import { SiVisualstudiocode } from "react-icons/si";
 
 export default function WithSubnavigation() {
-  const { scrolling, scrollTop } = useScrollPosition()
+	const { scrolling, scrollTop } = useScrollPosition();
 
-  const { isOpen, onToggle } = useDisclosure()
+	const { isOpen, onToggle } = useDisclosure();
 
-  return (
-    <Box
-      pos='fixed'
-      w={'100%'}
-      id='navbar-container'
-      bg={(scrolling && scrollTop !== 0) || isOpen ? '#0c0e13' : 'transparent'}
-      transition={'all 300ms'}
-      zIndex={99}
-      shadow={scrolling || scrollTop === 0 || isOpen ? 'md' : 'none'}
-    >
-      <ScaleFade in={scrolling || scrollTop === 0 || isOpen}>
-        <Container maxWidth={'10xl'}>
-          <Flex color='white' minH={'60px'} align={'center'} py={'4'} px={'2'}>
-            <Flex
-              flex={{ base: 1, md: 'auto' }}
-              ml={{ base: -2 }}
-              display={{ base: 'flex', md: 'none' }}
-            >
-              <IconButton
-                onClick={onToggle}
-                icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={6} h={6} />}
-                variant={'ghost'}
-                aria-label={'Toggle Navigation'}
-              />
-            </Flex>
-            <Flex flex={{ base: 1 }} justify={{ base: 'end', md: 'start' }}>
-              <Flex display={'flex'} align='center' mt={'5px'}>
-                <Fade>
-                  <Link
-                    textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-                    fontFamily={'heading'}
-                    href='https://franco-carrizo.ar'
-                    title='My portfolio'
-                  >
-                    <Icon
-                      fontSize={'1.5em'}
-                      as={SiVisualstudiocode}
-                      color='pink.400'
-                      _hover={{ backgroundColor: 'gray.900', transition: 'all 300ms' }}
-                      transition='all 300ms'
-                    />
-                  </Link>
-                </Fade>
-              </Flex>
-              <Flex
-                display={{ base: 'none', md: 'flex' }}
-                width='100%'
-                justifyContent='end'
-                ml={10}
-              >
-                <DesktopNav />
-              </Flex>
-            </Flex>
-          </Flex>
-        </Container>
-      </ScaleFade>
-      <Collapse in={isOpen} animateOpacity>
-        <MobileNav onToggle={onToggle} />
-      </Collapse>
-    </Box>
-  )
+	return (
+		<Box
+			pos="fixed"
+			w={"100%"}
+			id="navbar-container"
+			bg={
+				(scrolling && scrollTop !== 0) || isOpen
+					? "#0c0e13"
+					: "transparent"
+			}
+			transition={"all 300ms"}
+			zIndex={99}
+			shadow={scrolling || scrollTop === 0 || isOpen ? "md" : "none"}
+		>
+			<ScaleFade in={scrolling || scrollTop === 0 || isOpen}>
+				<Container maxWidth={"10xl"}>
+					<Flex
+						color="white"
+						minH={"60px"}
+						align={"center"}
+						py={"4"}
+						px={"2"}
+					>
+						<Flex
+							flex={{ base: 1, md: "auto" }}
+							ml={{ base: -2 }}
+							display={{ base: "flex", md: "none" }}
+						>
+							<IconButton
+								onClick={onToggle}
+								color="blackAlpha.700"
+								icon={
+									isOpen ? (
+										<CloseIcon
+											color={"white"}
+											w={3}
+											h={3}
+										/>
+									) : (
+										<HamburgerIcon w={6} h={6} />
+									)
+								}
+								variant={"ghost"}
+								aria-label={"Toggle Navigation"}
+							/>
+						</Flex>
+						<Flex
+							flex={{ base: 1 }}
+							justify={{ base: "end", md: "start" }}
+						>
+							<Flex display={"flex"} align="center" mt={"5px"}>
+								<Fade>
+									<Link
+										textAlign={useBreakpointValue({
+											base: "center",
+											md: "left",
+										})}
+										fontFamily={"heading"}
+										href="https://franco-carrizo.ar"
+										title="My portfolio"
+									>
+										<Icon
+											fontSize={"1.5em"}
+											as={SiVisualstudiocode}
+											color="pink.400"
+										/>
+									</Link>
+								</Fade>
+							</Flex>
+							<Flex
+								display={{ base: "none", md: "flex" }}
+								width="100%"
+								justifyContent="end"
+								ml={10}
+							>
+								<DesktopNav />
+							</Flex>
+						</Flex>
+					</Flex>
+				</Container>
+			</ScaleFade>
+			<Collapse in={isOpen} animateOpacity>
+				<MobileNav onToggle={onToggle} />
+			</Collapse>
+		</Box>
+	);
 }
 
 const DesktopNav = () => {
-  return (
-    <Stack direction={'row'} spacing={4}>
-      <Fade cascade>
-        {NAV_ITEMS.map((navItem) => (
-          <Flex p={2} key={navItem.label} id={`nav-${navItem.number}`}>
-            <Text fontSize={'sm'} fontFamily={'Roboto Mono'} color={'pink.400'} me={1}>
-              {navItem.number}.
-            </Text>
+	return (
+		<Stack direction={"row"} spacing={4}>
+			<Fade cascade>
+				{NAV_ITEMS.map((navItem) => (
+					<Flex
+						p={2}
+						key={navItem.label}
+						id={`nav-${navItem.number}`}
+					>
+						<Text
+							fontSize={"sm"}
+							fontFamily={"Roboto Mono"}
+							color={"pink.300"}
+							me={1}
+						>
+							{navItem.number}.
+						</Text>
 
-            <Link
-              href={navItem.href ?? '#'}
-              fontSize={'sm'}
-              fontWeight={500}
-              fontFamily={'Roboto Mono'}
-              color={'white'}
-              _hover={{
-                textDecoration: 'none',
-                color: 'pink.400'
-              }}
-            >
-              {navItem.label}
-            </Link>
-          </Flex>
-        ))}
-      </Fade>
-    </Stack>
-  )
-}
+						<Link
+							href={navItem.href ?? "#"}
+							fontSize={"sm"}
+							fontWeight={500}
+							fontFamily={"Roboto Mono"}
+							color={"black"}
+							_hover={{
+								color: "pink.300",
+							}}
+						>
+							{navItem.label}
+						</Link>
+					</Flex>
+				))}
+			</Fade>
+		</Stack>
+	);
+};
 
 const MobileNav = ({ onToggle }) => {
-  return (
-    <Stack bg={'#0c0e13'} p={4} display={{ md: 'none' }}>
-      {NAV_ITEMS.map((navItem) => (
-        <MobileNavItem key={navItem.label} {...navItem} onToggle={onToggle} />
-      ))}
-    </Stack>
-  )
-}
+	return (
+		<Stack bg={"blackAlpha.800"} p={4} display={{ md: "none" }}>
+			{NAV_ITEMS.map((navItem) => (
+				<MobileNavItem
+					key={navItem.label}
+					{...navItem}
+					onToggle={onToggle}
+				/>
+			))}
+		</Stack>
+	);
+};
 
 const MobileNavItem = ({ label, href, onToggle }) => {
-  return (
-    <Stack spacing={4}>
-      <Flex
-        py={2}
-        as={Link}
-        href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
-        _hover={{
-          textDecoration: 'none'
-        }}
-      >
-        <Text fontWeight={600} color={'whiteAlpha.900'} onClick={onToggle}>
-          {label}
-        </Text>
-      </Flex>
-    </Stack>
-  )
-}
+	return (
+		<Stack spacing={4}>
+			<Flex
+				py={2}
+				as={Link}
+				href={href ?? "#"}
+				justify={"space-between"}
+				align={"center"}
+				_hover={{
+					textDecoration: "none",
+				}}
+			>
+				<Text
+					fontWeight={600}
+					color={"whiteAlpha.900"}
+					onClick={onToggle}
+					transition="all 300ms"
+					_hover={{ color: "pink.400", transition: "all 300ms" }}
+				>
+					{label}
+				</Text>
+			</Flex>
+		</Stack>
+	);
+};
 
 const NAV_ITEMS = [
-  {
-    label: 'About me',
-    number: '01',
-    href: '#about-me'
-  },
-  {
-    label: 'Experience',
-    number: '02',
-    href: '#experience'
-  },
-  {
-    label: 'Work',
-    href: '#work',
-    number: '03'
-  },
-  {
-    label: 'Contact',
-    href: '#contact',
-    number: '04'
-  }
-]
+	{
+		label: "About me",
+		number: "01",
+		href: "#about-me",
+	},
+	{
+		label: "Experience",
+		number: "02",
+		href: "#experience",
+	},
+	{
+		label: "Work",
+		href: "#work",
+		number: "03",
+	},
+	{
+		label: "Contact",
+		href: "#contact",
+		number: "04",
+	},
+];
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,74 +1,91 @@
-import { Box, Link, List, ListIcon, ListItem, Text } from '@chakra-ui/react'
-import { RiInstagramLine, RiGithubLine, RiLinkedinLine } from 'react-icons/ri'
-import { useStaticQuery, graphql } from 'gatsby'
-import * as React from 'react'
-import NavBar from './Navbar'
-import './layout.css'
-import { Fade } from 'react-awesome-reveal'
-import SEO from './seo'
+import { Box, Link, List, ListIcon, ListItem, Text } from "@chakra-ui/react";
+import { RiInstagramLine, RiGithubLine, RiLinkedinLine } from "react-icons/ri";
+import { useStaticQuery, graphql } from "gatsby";
+import * as React from "react";
+import NavBar from "./Navbar";
+import "./layout.css";
+import { Fade } from "react-awesome-reveal";
+import SEO from "./seo";
 
 const Layout = ({ pageTitle, children }) => {
-  return (
-    <Box bgColor={'gray.900'} color='whiteAlpha.900' minH='100vh'>
-      <SEO title='Franco Carrizo' description='My personal portfolio' />
+	return (
+		<Box bgColor={"white.900"} color="gray.900" minH="100vh">
+			<SEO title="Franco Carrizo" description="My personal portfolio" />
 
-      <NavBar />
-      <Fade triggerOnce delay={2000}>
-        <div className='line-left'>
-          <List className='ul' display={'flex'} flexDirection={'column'}>
-            <ListItem margin={'5px auto'}>
-              <Link href='https://www.instagram.com/francarrizo13/' isExternal>
-                <ListIcon
-                  fontSize={'xl'}
-                  as={RiInstagramLine}
-                  margin={'5px auto'}
-                  color={'whiteAlpha.800'}
-                ></ListIcon>
-              </Link>
-            </ListItem>
-            <ListItem margin={'5px auto'}>
-              <Link href='https://github.com/FrancoCarrizoDev' isExternal>
-                <ListIcon
-                  fontSize={'xl'}
-                  as={RiGithubLine}
-                  margin={'5px auto'}
-                  color={'whiteAlpha.800'}
-                ></ListIcon>
-              </Link>
-            </ListItem>
-            <ListItem margin={'5px auto 15px auto'}>
-              <Link href='https://www.linkedin.com/in/francocarrizodev/' isExternal>
-                <ListIcon
-                  fontSize={'xl'}
-                  as={RiLinkedinLine}
-                  margin={'5px auto 15px auto'}
-                  color={'whiteAlpha.800'}
-                ></ListIcon>
-              </Link>
-            </ListItem>
-          </List>
-        </div>
-      </Fade>
-      <Fade triggerOnce delay={2500}>
-        <div className='line-right'>
-          <List className='ul' display={'flex'} flexDirection={'column'}>
-            <ListItem>
-              <Text
-                fontFamily={'Roboto mono'}
-                fontSize={'sm'}
-                id='float-label-email'
-                display={'flex'}
-                marginBottom={'200px'}
-                color={'whiteAlpha.800'}
-              >
-                francoadrianc@gmail.com
-              </Text>
-            </ListItem>
-          </List>
-        </div>
-      </Fade>
-      <main id='main'>{children}</main>
-    </Box>
-  )
-}
-export default Layout
+			<NavBar />
+			<Fade triggerOnce delay={2000}>
+				<div className="line-left">
+					<List
+						className="ul"
+						display={"flex"}
+						flexDirection={"column"}
+					>
+						<ListItem margin={"5px auto"}>
+							<Link
+								href="https://www.instagram.com/francarrizo13/"
+								isExternal
+							>
+								<ListIcon
+									fontSize={"xl"}
+									as={RiInstagramLine}
+									margin={"5px auto"}
+									color={"whiteAlpha.800"}
+								></ListIcon>
+							</Link>
+						</ListItem>
+						<ListItem margin={"5px auto"}>
+							<Link
+								href="https://github.com/FrancoCarrizoDev"
+								isExternal
+							>
+								<ListIcon
+									fontSize={"xl"}
+									as={RiGithubLine}
+									margin={"5px auto"}
+									color={"whiteAlpha.800"}
+								></ListIcon>
+							</Link>
+						</ListItem>
+						<ListItem margin={"5px auto 15px auto"}>
+							<Link
+								href="https://www.linkedin.com/in/francocarrizodev/"
+								isExternal
+							>
+								<ListIcon
+									fontSize={"xl"}
+									as={RiLinkedinLine}
+									margin={"5px auto 15px auto"}
+									color={"whiteAlpha.800"}
+								></ListIcon>
+							</Link>
+						</ListItem>
+					</List>
+				</div>
+			</Fade>
+			<Fade triggerOnce delay={2500}>
+				<div className="line-right">
+					<List
+						className="ul"
+						display={"flex"}
+						flexDirection={"column"}
+					>
+						<ListItem>
+							<Text
+								fontFamily={"Roboto mono"}
+								fontSize={"sm"}
+								id="float-label-email"
+								display={"flex"}
+								marginBottom={"200px"}
+								color={"whiteAlpha.800"}
+							>
+								francoadrianc@gmail.com
+							</Text>
+						</ListItem>
+					</List>
+				</div>
+			</Fade>
+			<main id="main">{children}</main>
+		</Box>
+	);
+};
+export default Layout;
